7598d038b97afba8063b02da3bcbeb88d9b48445
Aung Thu Moe <aung.thumoe@brainmates.com.au>
2024-12-15 22:59:11 -0600

    update internationalization

--- src/main/java/edu/miu/cs544/moe/emr/application/GlobalControllerExceptionHandler.java
+++ src/main/java/edu/miu/cs544/moe/emr/application/GlobalControllerExceptionHandler.java
@@ -2,15 +2,14 @@ package edu.miu.cs544.moe.emr.application;
 
 import edu.miu.cs544.moe.emr.application.dto.ErrorDto;
 import edu.miu.cs544.moe.emr.application.exception.NotFoundException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
-@ControllerAdvice(annotations = RestController.class, basePackages = "edu.miu.cs544.moe.emr")
+@ControllerAdvice(basePackages = "edu.miu.cs544.moe.emr")
 public class GlobalControllerExceptionHandler {
     @ExceptionHandler(NotFoundException.class)
     @ResponseBody
+    @ResponseStatus(HttpStatus.NOT_FOUND)
     public ErrorDto handleNotException(NotFoundException e) {
         return new ErrorDto(e.getMessage());
     }
